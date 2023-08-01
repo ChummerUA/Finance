4254d9a81a8d39ec3887ca616ca173f2d0fbf97c
Herman <chummer1008@gmail.com>
2023-08-01 18:01:32 +0300

    Add mono httpclient definition

---
+++ network/src/main/java/com/chummer/finance/network/monobank/NetworkDefinitions.kt
@@ -0,0 +1,16 @@
+package com.chummer.finance.network.monobank
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+
+const val baseUrl = "https://api.monobank.ua/"
+
+fun provideMonoClient(
+    token: String
+) = HttpClient {
+    defaultRequest {
+        url(baseUrl)
+        header("X-Token", token)
+    }
+}
