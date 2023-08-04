1253ff7db2107ed0c494985815b382b1b38c87fa
Herman <chummer1008@gmail.com>
2023-08-04 17:53:51 +0300

    Implement upsert jars usecase

---
+++ db/src/main/java/com/chummer/finance/db/mono/UpsertJarsUseCase.kt
@@ -0,0 +1,25 @@
+package com.chummer.finance.db.mono
+
+import app.cash.sqldelight.TransactionWithoutReturn
+import com.chummer.infrastructure.db.useCases.transaction.withoutResult.DbTransactionUseCase
+import mono.Jar
+import mono.JarQueries
+
+class UpsertJarsUseCase(
+    queries: JarQueries
+) : DbTransactionUseCase<List<Jar>, JarQueries>(KEY, queries) {
+    override fun TransactionWithoutReturn.execute(argument: List<Jar>) {
+        argument.forEach {
+            transacter.upsertJar(
+                id = it.id,
+                title = it.title,
+                description = it.description,
+                currencyCode = it.currency_code,
+                balance = it.balance,
+                goal = it.goal
+            )
+        }
+    }
+}
+
+private const val KEY = "upsert_jars"
